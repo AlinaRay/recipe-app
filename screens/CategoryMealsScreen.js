82878d4cf66063d79469524d4d6fe096387966a4
Alina Ray <a.alina.ray@gmail.com>
2019-10-17 18:28:45 +0300

    navigation was added

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
 const CategoryMealsScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>The CategoryMeals Screen!</Text>
+            <Button title="Go to Details" onPress={()=> {
+                props.navigation.navigate({routeName: 'MealDetail'})
+            }} />
+            <Button title="back" onPress={()=> {
+                props.navigation.pop();
+            }}/>
         </View>
     )
 };
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
 const MealDetailScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>The Meal Screen!</Text>
+            <Button title='go back'
+                    onPress={()=> {
+                        props.navigation.popToTop();
+                    }}
+            />
         </View>
     )
 };
