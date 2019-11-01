1c9057697891842ded044d26b7a3ba6ce3194f2e
Alina Ray <a.alina.ray@gmail.com>
2019-11-01 15:11:50 +0200

    redux store was added

--- App.js
+++ App.js
@@ -1,42 +1,43 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
-import { useScreens } from 'react-native-screens';
-
+import {AppLoading} from 'expo';
+import {useScreens} from 'react-native-screens';
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
 import MealsNavigator from './navigation/MealsNavigator';
+import mealsReducer from './store/reducers/meals';
 
 useScreens();
 
+const rootReducer = combineReducers({
+    meals: mealsReducer
+});
+
+const store = createStore(rootReducer);
+
 const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-  });
+    return Font.loadAsync({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
 };
 
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
+    const [fontLoaded, setFontLoaded] = useState(false);
 
-  if (!fontLoaded) {
+    if (!fontLoaded) {
+        return (
+            <AppLoading
+                startAsync={fetchFonts}
+                onFinish={() => setFontLoaded(true)}
+            />
+        );
+    }
+
     return (
-        <AppLoading
-            startAsync={fetchFonts}
-            onFinish={()=> setFontLoaded(true)}
-        />
+        <Provider store={store}>
+            <MealsNavigator />
+        </Provider>
     );
-  }
-
-  return (
-    <MealsNavigator />
-  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
